053b777dfdddd2c31f46ce015931fcb9d714b98f
Kirill <pavlovkirill322@gmail.com>
2021-12-24 18:25:10 +0300

    until it comes out to do pagination, I'll leave it for later

--- src/pug/blocks/pagination/pagination.js
+++ src/pug/blocks/pagination/pagination.js
@@ -3,38 +3,64 @@ import './pagination.scss';
 const paginationButtons = document.querySelector('.pagination__list');
 const paginationDescription = document.querySelector('.pagination__descr');
 
-let totalPages = 120;
+let totalItems = 153;
+let currentPage = 3;
 
-function paginationRefresh(totalPages, page) {
-  let liTag = '';
-  let activeLi;
-  let beforePages = page - 1;
-  let afterPages = page + 1;
+paginate(totalItems, currentPage);
 
-  if (beforePages == 0) {
-    beforePages == page;
+function paginate(totalItems, currentPage) {
+  let pageSize = 12;
+  let totalPages = Math.ceil(totalItems / pageSize);
+  let page = '';
+  let activePage;
+  let beforePages = currentPage - 2;
+  let afterPages = currentPage + 2;
+
+  if (currentPage < 1) {
+    currentPage = 1;
+  } else if (currentPage > totalPages) {
+    currentPage = totalPages;
+  } else if (currentPage == 1) {
+    beforePages = currentPage;
   }
 
-  if (page > 1) {
-    liTag += `<li class="pagination__prev"><img class="pagination__img pagination__img--left" src="../../../images/arrow_button.svg" alt="Prev" /></li>`;
+  if (currentPage > 1) {
+    page += `<li class="pagination__prev"><img class="pagination__img pagination__img--left" src="../../../images/arrow_button.svg" alt="Prev" /></li>`;
   }
-  
+
   for (let pageLenght = beforePages; pageLenght <= afterPages; pageLenght++) {
-    if (page == pageLenght) {
-      activeLi = 'pagination__number--active';
+    if (currentPage == pageLenght) {
+      activePage = 'pagination__number--active';
     } else {
-      activeLi = '';
+      activePage = '';
     }
-    liTag += `<li class="pagination__number ${activeLi}">${pageLenght}</li>`;
+    page += `<li class="pagination__number ${activePage}">${pageLenght}</li>`;
   }
 
-  if (page < totalPages) {
-    liTag += `<li class="pagination__next"><img class="pagination__img pagination__img--right" src="../../../images/arrow_button.svg" alt="Next"></li>`;
+  if (currentPage < totalPages) {
+    page += `<li class="pagination__next"><img class="pagination__img pagination__img--right" src="../../../images/arrow_button.svg" alt="Next"></li>`;
   }
-  paginationButtons.innerHTML = liTag;
-  paginationDescription.innerHTML = `${page - (page % 12)} - ${
-    page - (page % 12) + 12
-  } из ${totalPages}+ вариантов аренды`;
+
+  console.log(beforePages, currentPage, afterPages);
+  paginationButtons.innerHTML = page;
+  paginationDescription.innerHTML = `${currentPage - (currentPage % 12)} - ${
+    currentPage - (currentPage % 12) + 12
+  } из 100+ вариантов аренды`;
+
+  paginationButtons.addEventListener('click', e => {
+    console.log(e.target.classList);
+    if (e.target.classList == 'pagination__number') {
+      // paginate(totalItems, +e.target.innerHTML);
+      console.log('lol');
+      console.log(e.target.classList, +e.target.innerText);
+    }
+
+    if (e.target.parentNode.classList == 'pagination__prev') {
+      paginate(totalItems, currentPage - 1);
+      console.log('done -');
+    } else if (e.target.parentNode.classList == 'pagination__next') {
+      paginate(totalItems, currentPage + 1);
+      console.log('done +');
+    }
+  });
 }
-
-paginationRefresh(totalPages, 55);
