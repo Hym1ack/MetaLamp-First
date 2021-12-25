84e820538531401d8dcc9d64658534a19b88486d
Kirill <pavlovkirill322@gmail.com>
2021-12-25 12:14:32 +0300

    Finished making button

---
+++ src/pug/blocks/text-field/text-field.js
@@ -0,0 +1,9 @@
+import './text-field.scss';
+
+// const Btns = document.querySelectorAll('.text-field__block');
+
+// Btns.forEach(btn => {
+//   btn.addEventListener('focus', () => {
+//     btn.classList.add('.text-field__focused');
+//   });
+// });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
 import '../styles/main.scss';
 
-import '../pug/blocks/colors/colors.js';
-import '../pug/blocks/type/type.js';
+// import '../pug/blocks/colors/colors.js';
+// import '../pug/blocks/type/type.js';
 
-import '../pug/blocks/checkbox/checkbox.js';
-import '../pug/blocks/radio/radio.js';
-import '../pug/blocks/toggle/toggle.js';
-import '../pug/blocks/rate/rate.js';
-import '../pug/blocks/like-btn/like-btn.js';
+// import '../pug/blocks/checkbox/checkbox.js';
+// import '../pug/blocks/radio/radio.js';
+// import '../pug/blocks/toggle/toggle.js';
+// import '../pug/blocks/rate/rate.js';
+// import '../pug/blocks/like-btn/like-btn.js';
 
-import '../pug/blocks/range-slider/range-slider.js';
-import '../pug/blocks/button/button.js';
-import '../pug/blocks/pagination/pagination.js';
+// import '../pug/blocks/range-slider/range-slider.js';
+// import '../pug/blocks/button/button.js';
+// import '../pug/blocks/pagination/pagination.js';
+
+// import '../pug/blocks/text-field/text-field.js';
