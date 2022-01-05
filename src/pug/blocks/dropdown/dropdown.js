80b0333da6565c952fe32dd0ea9b02876c66e705
Kirill <pavlovkirill322@gmail.com>
2022-01-06 01:52:46 +0300

    Refactoring dropdown btns, added choose with type

--- src/pug/blocks/dropdown/dropdown.js
+++ src/pug/blocks/dropdown/dropdown.js
@@ -1,71 +1,167 @@
 import './dropdown.scss';
 
-const dropdownMenu = document.querySelector('.dropdown__menu'),
-  dropdownBtn = document.querySelector('.dropdown__btn'),
-  dropdownCounter = dropdownMenu.querySelectorAll('.dropdown__item-counter'),
-  dropdownReset = dropdownMenu.querySelector('.dropdown__reset'),
-  dropdownApply = dropdownMenu.querySelector('.dropdown__apply');
+const dropdownArray = document.querySelectorAll('.dropdown');
 
-let currentNumbers = [];
+dropdownArray.forEach(dropdownArr => {
+  const dropdownBtn = dropdownArr.querySelector('.dropdown__btn'),
+    dropdownCounter = dropdownArr.querySelectorAll('.dropdown__item-counter'),
+    dropdownResetBtn = dropdownArr.querySelector('.dropdown__reset'),
+    dropdownApplyBtn = dropdownArr.querySelector('.dropdown__apply');
 
+  let currentNumbers = [];
 
-dropdownCounter.forEach(dropdownCount => {
-  dropdownCount.addEventListener('click', e => {
-    currentNumbers = [];
+  dropdownCounter.forEach(dropdownCount => {
+    dropdownCount.addEventListener('click', e => {
+      const target = e.target;
+      currentNumbers = [];
+      if (target.classList == 'dropdown__item-minus') {
+        target.nextSibling.innerText = Number(target.nextSibling.innerText) - 1;
 
-    const target = e.target;
-    if (target.classList == 'dropdown__item-minus') {
-      target.nextSibling.innerText = Number(target.nextSibling.innerText) - 1;
-      updateNumbers(currentNumbers, dropdownCounter);
-    } else if (target.classList == 'dropdown__item-plus') {
-      target.previousSibling.innerText =
-        Number(target.previousSibling.innerText) + 1;
-      updateNumbers(currentNumbers, dropdownCounter);
+        updateNumbers(currentNumbers);
+      } else if (target.classList == 'dropdown__item-plus') {
+        target.previousSibling.innerText =
+          Number(target.previousSibling.innerText) + 1;
+        updateNumbers(currentNumbers);
+      }
+    });
+
+    updateNumbers(currentNumbers);
+    function updateNumbers(currentNumbers) {
+      dropdownCounter.forEach(dropdownCount => {
+        currentNumbers.push(Number(dropdownCount.children[1].innerText));
+        if (dropdownCount.children[1].innerText == 0) {
+          dropdownCount.firstChild.classList.add('dropdown__item--opacity');
+        } else {
+          dropdownCount.firstChild.classList.remove('dropdown__item--opacity');
+        }
+      });
+      if (dropdownBtn.classList.contains('dropdown__rooms')) {
+        let outerText = '';
+        const arrNames = ['спальни', 'кровати', 'ванные комнаты'];
+
+        for (let i = 0; i < arrNames.length; i++) {
+          outerText += `${currentNumbers[i]} ${arrNames[i]}, `;
+        }
+
+        dropdownBtn.innerText = outerText;
+        if (dropdownBtn.innerText.length > 19) {
+          dropdownBtn.innerText = `${outerText.substring(0, 20)}...`;
+        }
+      }
+    }
+
+    if (!dropdownBtn.classList.contains('dropdown__rooms')) {
+      dropdownResetBtn.addEventListener('click', () => {
+        currentNumbers = [0];
+        dropdownCounter.forEach(dropdownCount => {
+          dropdownCount.children[1].innerText = currentNumbers;
+        });
+        dropdownBtn.innerText = `Выберите количество гостей`;
+        updateNumbers(currentNumbers, dropdownCounter);
+      });
+
+      dropdownApplyBtn.addEventListener('click', () => {
+        const guests = currentNumbers.reduce(
+          (previousValue, currentValue) => previousValue + currentValue
+        );
+        switch (guests) {
+          case 0:
+            dropdownBtn.innerText = `Выберите количество гостей`;
+            break;
+          case 1:
+            dropdownBtn.innerText = `${guests} гость`;
+            break;
+          case 2:
+          case 3:
+          case 4:
+            dropdownBtn.innerText = `${guests} гостя`;
+            break;
+
+          default:
+            dropdownBtn.innerText = `${guests} гостей`;
+            break;
+        }
+      });
     }
   });
 });
 
-dropdownReset.addEventListener('click', () => {
-  currentNumbers = [0];
-  dropdownCounter.forEach(dropdownCount => {
-    dropdownCount.children[1].innerText = currentNumbers;
-  });
-  dropdownBtn.innerText = `Выберите количество гостей`;
-  updateNumbers(currentNumbers, dropdownCounter);
-});
+// const dropdownMenuArr = document.querySelectorAll('.dropdown__menu');
 
-dropdownApply.addEventListener('click', () => {
-  const guests = currentNumbers.reduce(
-    (previousValue, currentValue) => previousValue + currentValue
-  );
-  switch (guests) {
-    case 0:
-      dropdownBtn.innerText = `Выберите количество гостей`;
-      break;
-    case 1:
-      dropdownBtn.innerText = `${guests} гость`;
-      break;
-    case 2:
-    case 3:
-    case 4:
-      dropdownBtn.innerText = `${guests} гостя`;
-      break;
+// dropdownMenuArr.forEach(dropdownMenu => {
+//   const dropdownBtn = document.querySelectorAll('.dropdown__btn'),
+//     dropdownCounter = dropdownMenu.querySelectorAll('.dropdown__item-counter'),
+//     dropdownResetAll = dropdownMenu.querySelectorAll('.dropdown__reset'),
+//     dropdownApplyAll = dropdownMenu.querySelectorAll('.dropdown__apply');
 
-    default:
-      dropdownBtn.innerText = `${guests} гостей`;
-      break;
-  }
-});
+//   let currentNumbers = [];
 
-updateNumbers(currentNumbers, dropdownCounter);
+//   dropdownCounter.forEach(dropdownCount => {
+//     dropdownCount.addEventListener('click', e => {
+//       currentNumbers = [];
 
-function updateNumbers(currentNumbers, dropdownCounter) {
-  dropdownCounter.forEach(dropdownCount => {
-    currentNumbers.push(Number(dropdownCount.children[1].innerText));
-    if (dropdownCount.children[1].innerText == 0) {
-      dropdownCount.firstChild.classList.add('dropdown__item--opacity');
-    } else {
-      dropdownCount.firstChild.classList.remove('dropdown__item--opacity');
-    }
-  });
-}
+//       const target = e.target;
+//       const targetRooms =
+//         e.target.parentNode.classList.contains('dropdown__rooms');
+
+//       if (target.classList == 'dropdown__item-minus') {
+//         target.nextSibling.innerText = Number(target.nextSibling.innerText) - 1;
+
+//         updateNumbers(currentNumbers, dropdownCounter);
+//       } else if (target.classList == 'dropdown__item-plus') {
+//         dropdownRoomsText(targetRooms, dropdownRooms);
+//         target.previousSibling.innerText =
+//           Number(target.previousSibling.innerText) + 1;
+//         updateNumbers(currentNumbers, dropdownCounter);
+//       }
+//     });
+//   });
+
+//   dropdownResetAll.forEach(dropdownReset => {
+//     dropdownReset.addEventListener('click', () => {
+//       currentNumbers = [0];
+//       dropdownCounter.forEach(dropdownCount => {
+//         dropdownCount.children[1].innerText = currentNumbers;
+//       });
+//       dropdownBtn.innerText = `Выберите количество гостей`;
+//       updateNumbers(currentNumbers, dropdownCounter);
+//     });
+//   });
+
+//   dropdownApplyAll.forEach(dropdownApply => {
+//     dropdownApply.addEventListener('click', () => {
+//       const guests = currentNumbers.reduce(
+//         (previousValue, currentValue) => previousValue + currentValue
+//       );
+//       switch (guests) {
+//         case 0:
+//           dropdownBtn.innerText = `Выберите количество гостей`;
+//           break;
+//         case 1:
+//           dropdownBtn.innerText = `${guests} гость`;
+//           break;
+//         case 2:
+//         case 3:
+//         case 4:
+//           dropdownBtn.innerText = `${guests} гостя`;
+//           break;
+
+//         default:
+//           dropdownBtn.innerText = `${guests} гостей`;
+//           break;
+//       }
+//     });
+//   });
+
+//   updateNumbers(currentNumbers, dropdownCounter);
+//   function updateNumbers(currentNumbers, dropdownCounter) {
+//     dropdownCounter.forEach(dropdownCount => {
+//       currentNumbers.push(Number(dropdownCount.children[1].innerText));
+//       if (dropdownCount.children[1].innerText == 0) {
+//         dropdownCount.firstChild.classList.add('dropdown__item--opacity');
+//       } else {
+//         dropdownCount.firstChild.classList.remove('dropdown__item--opacity');
+//       }
+//     });
+//   }
+// // });
