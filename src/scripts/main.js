1e145e94b6f1f063b6081b6fbcd95f95eaf88842
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-01-07 21:49:25 +0300

    Make comment block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,20 @@ import '../styles/main.scss';
 // import '../pug/blocks/colors/colors.js';
 // import '../pug/blocks/type/type.js';
 
-import '../pug/blocks/checkbox/checkbox.js';
+// import '../pug/blocks/checkbox/checkbox.js';
 // import '../pug/blocks/radio/radio.js';
 // import '../pug/blocks/toggle/toggle.js';
 // import '../pug/blocks/rate/rate.js';
-// import '../pug/blocks/like-btn/like-btn.js';
+import '../pug/blocks/like-btn/like-btn.js';
 
 // import '../pug/blocks/range-slider/range-slider.js';
-// import '../pug/blocks/button/button.js';
-// import '../pug/blocks/pagination/pagination.js';
+import '../pug/blocks/button/button.js';
+import '../pug/blocks/pagination/pagination.js';
 
-// import '../pug/blocks/text-field/text-field.js';
+import '../pug/blocks/text-field/text-field.js';
 // import '../pug/blocks/dropdown/dropdown.js';
 // import '../pug/blocks/date-dropdown/date-dropdown.js';
+
 // import '../pug/blocks/expandable-checkbox/expandable-checkbox.js';
 // import '../pug/blocks/bullet-list/bullet-list.js';
+import '../pug/blocks/comment/comment';
