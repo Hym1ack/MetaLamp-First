98ba8547b666932895797f9da55ef6f7de48712f
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-01-07 18:59:31 +0300

    added rich checkboxes, and added mixins

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@ import '../styles/main.scss';
 // import '../pug/blocks/colors/colors.js';
 // import '../pug/blocks/type/type.js';
 
-// import '../pug/blocks/checkbox/checkbox.js';
+import '../pug/blocks/checkbox/checkbox.js';
 // import '../pug/blocks/radio/radio.js';
 // import '../pug/blocks/toggle/toggle.js';
 // import '../pug/blocks/rate/rate.js';
@@ -17,4 +17,4 @@ import '../styles/main.scss';
 // import '../pug/blocks/dropdown/dropdown.js';
 // import '../pug/blocks/date-dropdown/date-dropdown.js';
 // import '../pug/blocks/expandable-checkbox/expandable-checkbox.js';
-import '../pug/blocks/bullet-list/bullet-list.js';
+// import '../pug/blocks/bullet-list/bullet-list.js';
