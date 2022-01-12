93b0497383b9d34afc33f13b129aee1eebcc2d7f
Kirill <pavlovkirill322@gmail.com>
2022-01-12 22:55:20 +0300

    added to text field arrow + mixin

--- src/pug/blocks/text-field/text-field.js
+++ src/pug/blocks/text-field/text-field.js
@@ -1,9 +1,1 @@
 import './text-field.scss';
-
-// const Btns = document.querySelectorAll('.text-field__block');
-
-// Btns.forEach(btn => {
-//   btn.addEventListener('focus', () => {
-//     btn.classList.add('.text-field__focused');
-//   });
-// });
