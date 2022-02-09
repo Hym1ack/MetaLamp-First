3f6f8b2124242eb4b91a221ee9c7fe78bfee2db3
Kirill <pavlovkirill322@gmail.com>
2022-02-09 15:20:33 +0300

    created form elements page

---
+++ src/pages/formElements/formElements.js
@@ -0,0 +1,22 @@
+import '../../styles/main.scss';
+import './formElements.scss';
+
+import '../../blocks/title/title';
+
+import '../../blocks/text-field/text-field';
+import '../../blocks/dropdown/dropdown';
+import '../../blocks/date-dropdown/date-dropdown';
+import '../../blocks/expandable-checkbox/expandable-checkbox';
+import '../../blocks/feature/feature'
+
+import '../../blocks/checkbox/checkbox';
+import '../../blocks/radio/radio';
+import '../../blocks/toggle/toggle';
+import '../../blocks/like-btn/like-btn';
+import '../../blocks/rate/rate';
+import '../../blocks/comment/comment'
+
+import '../../blocks/range-slider/range-slider';
+import '../../blocks/button/button';
+import '../../blocks/pagination/pagination';
+import '../../blocks/bullet-list/bullet-list'
