06a6e181bf71974a1727f451ae1d1b229f344721
Kirill <pavlovkirill322@gmail.com>
2022-02-09 15:22:01 +0300

    deleted comments and fixed a bug

--- src/blocks/expandable-checkbox/expandable-checkbox.js
+++ src/blocks/expandable-checkbox/expandable-checkbox.js
@@ -1,13 +1,12 @@
 import './expandable-checkbox.scss';
 
 const expandCheckboxBtns = document.querySelectorAll('.expand-checkbox__btn'),
-  expandCheckboxImg = document.querySelector('.expand-checkbox__img'),
+  expandCheckboxImg = document.querySelectorAll('.expand-checkbox__img'),
   expandCheckboxLists = document.querySelectorAll('.expand-checkbox__list');
 
 expandCheckboxBtns.forEach((checkboxBtn, i) => {
   checkboxBtn.addEventListener('click', () => {
     expandCheckboxLists[i].classList.toggle('expand-checkbox__hide-smooth');
-    expandCheckboxImg.classList.toggle('expand-checkbox__img--rotate');
-    // console.log(expandCheckboxImg);
+    expandCheckboxImg[i].classList.toggle('expand-checkbox__img--rotate');
   });
 });
